de82acb4ce5f8f24e2be8f1e353d8f292aca63a7
racie7 <graciamuts@gmail.com>
2022-08-06 19:49:34 +0300

    Done

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(ride){
+  let message
+  if (ride <= 400){
+    message = 'This one is on me!';
+   }
+   if (ride > 200 && ride < 4000 ){
+    message = 'That will be twenty bucks.';
+   }
+   if (ride > 2000 && ride <= 2500 ){
+    message = 'I will gladly take your thirty bucks.';
+   }
+   if (ride > 2500 ){
+    message = 'No can do.';
+   }
+
+   return message
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  let message
+  if(city == 'NYC'){
+    message = 'Ok, sounds good.';
+  }
+  else {
+    message = 'No go.';
+  }
+ 
+  return message
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+
+function switchOnCharmFromTip(tip){
+  let message
+  if(tip == 'generous'){
+    message = 'Thank you so much.';
+  }
+  else if(tip == 'not as generous'){
+    message = 'Thank you.';
+  }
+  else{
+    message = 'Bye.'
+  }
+  return message;
 }
